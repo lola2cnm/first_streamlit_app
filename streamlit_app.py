981bcab095c383190e85b740a0d1d85684388db6
lola2cnm <106263204+lola2cnm@users.noreply.github.com>
2022-06-02 09:58:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,6 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
       
-except URLError as e:
-    streamlit.error()
-
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
